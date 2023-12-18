7d683b6a04f7226904ac31be185740a5eae90a85
noahligala <63068195+noahligala@users.noreply.github.com>
2023-12-19 02:30:15 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,10 +2,10 @@
 stack_t *head = NULL;
 
 /**
- * main - main  entry point.
- * @argc: arguments count.
- * @argv: list of arguments.
- * Return: always 0.
+ * main - entry point
+ * @argc: arguments count
+ * @argv: list of arguments
+ * Return: always 0
  */
 
 int main(int argc, char *argv[])
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
+		exit_with_error(1);
 	}
 	open_file(argv[1]);
 	free_nodes();
@@ -21,43 +21,62 @@ int main(int argc, char *argv[])
 }
 
 /**
- * free_nodes - frees all nodes.
- * Return: Nothing.
- */
-void free_nodes(stack_t **head)
-{
-	stack_t *current = *head;
-	stack_t *next;
-
-	while (current != NULL)
-	{
-		next = current->next;
-		free(current);
-		current = next;
-	}
-
-	*head = NULL;
-}
-
-/**
- * create_node - creates a new node.
- * @n: integer to go into the node.
- * Return: A pointer to the new node on success else 0.
+ * create_node - Creates a node.
+ * @n: Number to go inside the node.
+ * Return: Upon success a pointer to the node. Otherwise NULL.
  */
 stack_t *create_node(int n)
 {
 	stack_t *node;
 
-	node = malloc(sixeof(stack_t));
+	node = malloc(sizeof(stack_t));
 	if (node == NULL)
-	{
-		perror("Error: Unable to allocate memory");
-		exit(EXIT_FAILURE);
-	}
-
-	node->n = n;
+		handle_file_error(4);
 	node->next = NULL;
 	node->prev = NULL;
-
+	node->n = n;
 	return (node);
+}
+
+/**
+ * free_nodes - Frees nodes in the stack.
+ */
+void free_nodes(void)
+{
+	stack_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
+
+
+/**
+ * add_to_queue - Adds a node to the queue.
+ * @new_node: Pointer to the new node.
+ * @ln: line number of the opcode.
+ */
+void add_to_queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+	tmp = head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = *new_node;
+	(*new_node)->prev = tmp;
 }
